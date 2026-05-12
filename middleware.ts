d9825f55ea660cee1df71b76c45cf00559025cf3
Jyotishman pathak <jyotishmanpathak11@gmail.com>
2026-05-12 20:16:33 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
-import { auth } from "./auth";
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
 
-export const middleware = auth;
+export const { auth: middleware } = NextAuth(authConfig);
 
 export const config = {
   matcher: [
